fe58b20146218670c1be22c4da25fb54650c37a1
braden curtis <bradenacurtis801@gmail.com>
2023-09-14 16:52:10 -0600

    added doc strings and comments to explain code in main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import pickle
 import re
 
 class ChessBoard:
+    """Represents a chessboard and handles game operations such as moves and display."""
     def __init__(self):
+        """Initialize the chessboard with the default setup."""
+        # Standard starting positions for a chess game.
         self.board = [
             [Rook(0), Knight(0), Bishop(0), Queen(0), King(0), Bishop(0), Knight(0), Rook(0)],
             [Pawn(0), Pawn(0), Pawn(0), Pawn(0), Pawn(0), Pawn(0), Pawn(0), Pawn(0)],
@@ -22,6 +25,7 @@ class ChessBoard:
                     self.board[row][col].position = (row, col)
 
     def display(self):
+        """Display the current state of the chessboard."""
         for i, row in enumerate(self.board, start=1):
             display_row = []
             for piece in row:
@@ -31,9 +35,23 @@ class ChessBoard:
         print("    A   B   C   D   E   F   G   H")
         
     def validateInput(self, move):
+        """
+    Validates the input move string to ensure it matches the pattern of a valid chess move.
+    
+    A valid move is of the format: 'A1-A2' where 'A1' and 'A2' are coordinates on the chessboard.
+    The function allows for case-insensitive input and optional spaces around the hyphen.
+    
+    Args:
+        move (str): The move string to be validated.
+    
+    Returns:
+        MatchObject: Returns a match object if the move is valid, otherwise returns None.
+    """
         return re.match(r'^[A-Ha-h][1-8]\s*-\s*[A-Ha-h][1-8]$', move, re.I)
+
             
     def run(self):
+        """Main game loop, handles input from the players and game progression."""
         player1 = True
         while True:
             if player1:
@@ -56,7 +74,7 @@ class ChessBoard:
             player1 = not player1
             
     def handleMove(self, move):
-        # implemented but needs to be tested
+        """Parse the move input and handles the move on the board."""
         source, destination = [x.strip() for x in move.split('-')]
         source_coord = self.convert_to_coord(source)
         destination_coord = self.convert_to_coord(destination)
@@ -66,12 +84,14 @@ class ChessBoard:
         self.isValidMove(source_coord,destination_coord)
         
     def isValidMove(self,src_cord,dest_cord):
+        """Check if the move from src_cord to dest_cord is valid according to chess rules."""
         # TODO
         srcObj = self.board[src_cord[0]][src_cord[1]]
         destObj = self.board[dest_cord[0]][dest_cord[1]]
         pass
     
     def convert_to_coord(self, notation):
+        """Convert the user-friendly notation (like 'E2') to board coordinates (like (1, 4))."""
         col_map = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
         col = col_map[notation[0].upper()]
         row = 8 - int(notation[1])  # 8 - row number to get 0-indexed row
@@ -79,6 +99,7 @@ class ChessBoard:
                 
             
     def quit(self):
+        """Quit the game, clearing the console and printing an exit message."""
         # Clear the console
         os.system('cls' if os.name == 'nt' else 'clear')
         # Print a friendly exit message
@@ -87,6 +108,7 @@ class ChessBoard:
         sys.exit()
         
     def save(self):
+        """Save the current game state to a file for later continuation."""
         # implemented but needs to be tested
         print("Optional TODO: this saves the same state")
         with open("chess_save.dat", "wb") as f:
@@ -95,6 +117,7 @@ class ChessBoard:
         
     @classmethod
     def load(cls):
+        """Load a previously saved game state from a file."""
         # implemented but needs to be tested
         try:
             with open("chess_save.dat", "rb") as f:
@@ -105,6 +128,7 @@ class ChessBoard:
 
     
     def reset(self):
+        """Reset the game state to the default starting position and restarts the game."""
         self.__init__()
         self.run()
 
