28856e70fca364048a8f08eecae58a2ef8a9b6c1
braden curtis <bradenacurtis801@gmail.com>
2023-09-14 16:45:42 -0600

    added input handler to decipher move input as well as other commands

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
 from chestPieces import Rook, Pawn, King, Bishop, Queen, Knight
-
-col = {
-    'A':0,
-    'B':1,
-    'C':2,
-    'D':3,
-    'E':4,
-    'F':5,
-    'G':6,
-    'H':7
-}
-
-pieces = [
-    Rook,
-    Pawn,
-    King,
-    Bishop,
-    Queen,
-    Knight
-]
+import sys,os
+import pickle
+import re
 
 class ChessBoard:
     def __init__(self):
@@ -46,21 +29,100 @@ class ChessBoard:
             print(f"{9-i} | " + ' | '.join(display_row) + ' |')
             print("---------------------------------")
         print("    A   B   C   D   E   F   G   H")
+        
+    def validateInput(self, move):
+        return re.match(r'^[A-Ha-h][1-8]\s*-\s*[A-Ha-h][1-8]$', move, re.I)
+            
+    def run(self):
+        player1 = True
+        while True:
+            if player1:
+                player = 1
+            else:
+                player = 2
+            resp = input(f'Player {player} turn:')
 
-    def get_coordinates(self, command):
-        source, destination = command.split('-')
-        source = (8-int(source[1]), ord(source[0].upper())-65)
-        destination = (8-int(destination[1]), ord(destination[0].upper())-65)
-        return source, destination
+            if resp == "quit":
+                self.quit()
+            elif resp == "save":
+                self.save()
+            elif self.validateInput(resp):
+                self.handleMove(resp)
+                self.display()
+            else:
+                print("Invalid input. Please provide a move in the format 'E2 – E4'.")
+
+            # This changes the player move after the current player makes a move
+            player1 = not player1
+            
+    def handleMove(self, move):
+        # implemented but needs to be tested
+        source, destination = [x.strip() for x in move.split('-')]
+        source_coord = self.convert_to_coord(source)
+        destination_coord = self.convert_to_coord(destination)
+        
+        # TODO: Use the coordinates to move the piece from source to destination in self.board
+        # ...
+        self.isValidMove(source_coord,destination_coord)
+        
+    def isValidMove(self,src_cord,dest_cord):
+        # TODO
+        srcObj = self.board[src_cord[0]][src_cord[1]]
+        destObj = self.board[dest_cord[0]][dest_cord[1]]
+        pass
     
-    def initBoard(self):
-        for piece,_col in enumerate(pieces):
-            newObj = piece().setPos(row=1,col=_col)
-            newObj = piece().setPos(row=8,col=_col)
+    def convert_to_coord(self, notation):
+        col_map = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+        col = col_map[notation[0].upper()]
+        row = 8 - int(notation[1])  # 8 - row number to get 0-indexed row
+        return (row, col)
+                
+            
+    def quit(self):
+        # Clear the console
+        os.system('cls' if os.name == 'nt' else 'clear')
+        # Print a friendly exit message
+        print("Thanks for playing! Goodbye!")
+        # Exit the program
+        sys.exit()
+        
+    def save(self):
+        # implemented but needs to be tested
+        print("Optional TODO: this saves the same state")
+        with open("chess_save.dat", "wb") as f:
+            pickle.dump(self, f)
+        print("Game has been saved!")
+        
+    @classmethod
+    def load(cls):
+        # implemented but needs to be tested
+        try:
+            with open("chess_save.dat", "rb") as f:
+                return pickle.load(f)
+        except FileNotFoundError:
+            print("No saved game found.")
+            return None
+
+    
+    def reset(self):
+        self.__init__()
+        self.run()
 
 
             
 
 if __name__ == "__main__":
-    game = ChessBoard()
-    game.display()
+    print("Welcome to Chess!")
+    choice = input("Do you want to load a saved game? (yes/no): ").strip().lower()
+
+    if choice == 'yes':
+        game = ChessBoard.load()
+        if game:
+            game.run()
+        else:
+            print("Starting a new game.")
+            game = ChessBoard()
+            game.run()
+    else:
+        game = ChessBoard()
+        game.run()
