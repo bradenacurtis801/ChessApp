d0e8ad16e5ecb385701a9d9095d7464c10a632cd
Ericratz <ericratz@hotmail.com>
2023-09-21 19:10:50 -0600

    Modified various sections and added King movement

--- chessPieceADT.py
+++ chessPieceADT.py
@@ -3,21 +3,21 @@ from abc import ABC, abstractmethod
 class ChessPiece(ABC):
 
     def __init__(self):
-        self.move_vector = [None,None]
-        self.position = [None,None]
+        self.position = (None,None)
         self.points = None
+        self.team = 0
      
     # @abstractmethod   
     def getPos(self):
         pass
     
     # @abstractmethod
-    def setPos(self, row: int, col: int):
+    def setPos(self, pos):
         pass
     
     # @abstractmethod
     def getMoveVector(self):
         pass
-        
-
-        
+    
+    def validateMove(self, dest_cord, board):
+        pass
--- chestPieces.py
+++ chestPieces.py
@@ -3,42 +3,102 @@ from chessPieceADT import ChessPiece
 class Pawn(ChessPiece):
     def __init__(self,team):
         self.name = 'p'
-        if team == 1: self.name = self.name.upper()
-        self.move_vector = [1,0]
-        self.position = [None,None]
+        self.team = team
+        if self.team == 0: self.name = self.name.upper()
+        self.position = []
+        
+    def getPos(self):
+        return self.position
+    
+    def setPos(self, newPos):
+        self.position = newPos
+        
+    def validateMove(self, dest_cord, board):
+        print("pawn validatemove entered!")
+        #generate possible moves
+        row = self.position[0]
+        col = self.position[1]
+        moves = []
+        print(self.team)
+        if self.team == 0:
+            srcObj = board[row+1][col]
+            if srcObj == None:
+                moves.append((row+1, col))
+            #TODO add checking for front corners and for out of bounds
+                srcObj2 = board[row+2][col]
+                if row == 1 and srcObj2 == None:
+                    moves.append((row+2,col))
+        #TODO add check for opposite side pawn
+        print(dest_cord)
+        if dest_cord in moves:
+            return True
+        return False
 
 class Rook(ChessPiece):
     def __init__(self,team):
         self.name = 'r'
-        if team == 1: self.name = self.name.upper()
+        self.team = team
+        if self.team == 0: self.name = self.name.upper()
         self.move_vector = 2*[]
         self.position = 2*[]
 
 class Knight(ChessPiece):
     def __init__(self,team):
         self.name = 'n'
-        if team == 1: self.name = self.name.upper()
+        self.team = team
+        if self.team == 0: self.name = self.name.upper()
         self.move_vector = 2*[]
         self.position = 2*[]
 
 class Queen(ChessPiece):
     def __init__(self,team):
         self.name = 'q'
-        if team == 1: self.name = self.name.upper()
+        self.team = team
+        if self.team == 0: self.name = self.name.upper()
         self.move_vector = 2*[]
         self.position = 2*[]
 
 class King(ChessPiece):
     def __init__(self,team):
         self.name = 'k'
-        if team == 1: self.name = self.name.upper()
-        self.move_vector = 2*[]
-        self.position = 2*[]
+        self.team = team
+        if self.team == 0: self.name = self.name.upper()
+        self.position = []
+    
+    def getPos(self):
+        return self.position
+    
+    def setPos(self, newPos):
+        self.position = newPos
+    
+    #a King can move one square in any direction
+    def validateMove(self, dest_cord, board):
+        #generate possible moves
+        row = self.position[0]
+        col = self.position[1]
+        moves = []
+        #check all squares around king (including the king)
+        for i in range(-1,2):
+            for j in range(-1,2):
+                try:
+                    srcObj = board[row+i][col+j]
+                    if srcObj == None:
+                        moves.append((row+i,col+j))
+                    else:
+                        if srcObj.team != self.team:
+                            moves.append((row+i, col+j))
+                except IndexError:
+                    pass
+        #check if the destination is one of the valid moves
+        if dest_cord in moves:
+            return True
+        return False
 
 class Bishop(ChessPiece):
     def __init__(self,team):
         self.name = 'b'
-        if team == 1: self.name = self.name.upper()
+        self.team = team
+        if self.team == 0: self.name = self.name.upper()
         self.move_vector = 2*[]
         self.position = 2*[]
 
